65e8836824d397541f4158dbecaf5e9a07fb0c37
jzubietas <jhonathanisaizubietasantos@gmail.com>
2025-05-23 08:00:23 -0500

    Add getters for nombre and descripcion; initialize project and classpath files

--- src/ecommerce/productos/Producto.java
+++ src/ecommerce/productos/Producto.java
@@ -15,5 +15,7 @@ public class Producto {
         this.stock = stock;
     }
 
+    public String getNombre() { return nombre; }
+    public String getDescripcion() { return descripcion; }
     public double getPrecio() { return precio; }
 }
